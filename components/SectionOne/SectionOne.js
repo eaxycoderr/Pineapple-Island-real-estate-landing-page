18697c169ae731b5e6aa04ed1ef4a3069ea95473
webdev0202 <webdev0202@yandex.com>
2022-04-12 09:30:58 +0100

    new commit

--- components/SectionOne/SectionOne.js
+++ components/SectionOne/SectionOne.js
@@ -1,3 +1,4 @@
+
 export default function SectionOne () {
   return (
     <div className="section-one-container">
@@ -17,9 +18,19 @@ export default function SectionOne () {
           <h2>About <br />Pineapple Island</h2>
         </div>
         <div className="section-one-mobile-slide">
+          <div className="left-arrow">
+            <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9 17L1 9L9 1" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+          </div>
           <p>
             Pineapple Island is the most sought-after  community in Ibadan that values and puts the needs of their clients first. It is a territory with loft complexes that creates a country life and homely feeling that is integrated with industrial development. A perfect, professional built  abode that gives you the true taste of beauty and happiness.
           </p>
+          <div className="right-arrow">
+            <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1L9 9L1 17" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+          </div>
         </div>
       </div>
     </div>
